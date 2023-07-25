1a01850ae0f6b35abebcdfe1a1912bd687ddc01f
yuqi <yuqi@test.com>
2023-07-25 10:11:29 +0800

    fix

---
+++ precache-manifest.9f3fbbeebbd2e98cd3c7176475279bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce9babb95b65071510d03798224249f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a13fbf7bde5dbd2e8dfc",
+    "url": "/static/css/2.572b55c0.chunk.css"
+  },
+  {
+    "revision": "a17acbb2bd1264f61170",
+    "url": "/static/css/main.c71bccc8.chunk.css"
+  },
+  {
+    "revision": "a13fbf7bde5dbd2e8dfc",
+    "url": "/static/js/2.7edeae3e.chunk.js"
+  },
+  {
+    "revision": "96e348c6a1c1c766cbdd74cfa1ffa784",
+    "url": "/static/js/2.7edeae3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17acbb2bd1264f61170",
+    "url": "/static/js/main.822fb43d.chunk.js"
+  },
+  {
+    "revision": "7ce6dbf5e96950aeea93",
+    "url": "/static/js/runtime-main.3b5adcf2.js"
+  }
+]);
